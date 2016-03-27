407c05ee2a93b83e4116b374cb9b1e9dfb9f210d
rijesh <rijesh@inzyte.com>
2016-03-27 20:29:11 +0530

    testing celerybeat working

--- price_track_api/settings.py
+++ price_track_api/settings.py
@@ -147,7 +147,7 @@ CELERY_RESULT_BACKEND = 'redis://localhost/1'
 CELERYBEAT_SCHEDULE = {
     'every-day-refresh': {
         'task': 'api.tasks.refresh',
-        'schedule': timedelta(days = 1),
+        'schedule': timedelta(minute = 1),
     },
 }
 
