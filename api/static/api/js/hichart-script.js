c3a763c1d8e776b7e945c3596d191695e8f2aa71
rijesh <rijesh@inzyte.com>
2016-04-03 22:13:32 +0530

    highchart formatted

--- api/static/api/js/hichart-script.js
+++ api/static/api/js/hichart-script.js
@@ -4,8 +4,8 @@ $(function () {
 														var k=new Array(),d;
 														var j =new Array();
 														id=$(id).attr("data")
-														console.log('http://127.0.0.1:8000/pricehistory/id='+id+'?format=json')
-								$.getJSON('https://pricetrack-api.herokuapp.com/pricehistory/id='+id+'?format=json', function(data) {
+														
+								$.getJSON('http://127.0.0.1:8000/pricehistory/id='+id+'?format=json', function(data) {
 										// Populate series
 										$.each(data,function(key,val)
 												{   
@@ -23,7 +23,6 @@ $(function () {
 														k.push(parseInt(val.price));
 														}
 														j.push(f);
-														console.log(k)
 														processed_json.push(k);
 														k=[]
 												});
@@ -39,17 +38,21 @@ $(function () {
 						},
 								
 										title: {
-												text: ""
+												text: "Price history"
 										},
 										xAxis: {
 											tickInterval: 5,
-												gridLineWidth: .1,
-												crosshair:true,
+												// gridLineWidth: .1,
+												// crosshair:true,
 												categories:j,
 												allowDecimals: true,
 												title: {
-																text: "date"
-																},
+															text: "date"
+														},
+												plotLines: [{
+														value: 0,
+														width: 1,
+													}]
 						
 				
 										},
@@ -58,24 +61,22 @@ $(function () {
 												title: {
 														text: "Price"
 												},
-												tickInterval: 1000,
+												tickInterval: 10,
 										plotLines: [{
-								value: 0,
-								width: .001,
-								color: '#808080'
-
-
-						}]
+														value: 0,
+														width: 1,
+													}]
 										},credits: {
 												enabled: false
 												 },
 
 										 
 
-										 tooltip: {
-	 
-						shared: true,
-						crosshairs: true
+										 tooltip: {crosshairs: [{
+            width: 1.5,
+            dashStyle: 'solid',
+            color: 'gray'
+        }, false]
 				},
 										series: [{
 												name: 'Price',
@@ -93,3 +94,77 @@ $(function () {
 						
 				});
 }
+/**
+ * Grid-light theme for Highcharts JS
+ * @author Torstein Honsi
+ */
+
+// Load the fonts
+Highcharts.createElement('link', {
+   href: '//fonts.googleapis.com/css?family=Dosis:400,600',
+   rel: 'stylesheet',
+   type: 'text/css'
+}, null, document.getElementsByTagName('head')[0]);
+
+Highcharts.theme = {
+   colors: ["#000000"],
+   // colors: ['7cb5ec','#0066FF', '#00CCFF'],
+   chart: {
+      backgroundColor: null,
+      style: {
+         fontFamily: "Dosis, sans-serif"
+      }
+   },
+   title: {
+      style: {
+         fontSize: '16px',
+         fontWeight: 'bold',
+         textTransform: 'uppercase'
+      }
+   },
+   tooltip: {
+      borderWidth: 0,
+      backgroundColor: 'rgba(219,219,216,0.8)',
+      shadow: false
+   },
+   legend: {
+      itemStyle: {
+         fontWeight: 'bold',
+         fontSize: '13px'
+      }
+   },
+   xAxis: {
+      gridLineWidth: 1,
+      labels: {
+         style: {
+            fontSize: '12px'
+         }
+      }
+   },
+   yAxis: {
+      minorTickInterval: 'auto',
+      title: {
+         style: {
+            textTransform: 'uppercase'
+         }
+      },
+      labels: {
+         style: {
+            fontSize: '12px'
+         }
+      }
+   },
+   plotOptions: {
+      candlestick: {
+         lineColor: '#404048'
+      }
+   },
+
+
+   // General
+   background2: '#c0c0c0'
+
+};
+
+// Apply the theme
+Highcharts.setOptions(Highcharts.theme);
