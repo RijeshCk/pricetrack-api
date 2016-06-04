4fd85a3b8788bf0cf14ba2f9bb9708c1c9b1f724
rijesh <rijesh@inzyte.com>
2016-06-04 17:51:21 +0530

    modelfield length increased from 200 to 400

--- api/models.py
+++ api/models.py
@@ -4,14 +4,14 @@ from jsonfield import JSONField
 import collections
 
 class ProductData(models.Model):
-	product_name = models.CharField(max_length=200,blank=True,)
+	product_name = models.CharField(max_length=400,blank=True,)
 	product_price =models.FloatField()
-	product_url = models.URLField(max_length=200,)
+	product_url = models.URLField(max_length=400,)
 	product_id = models.CharField(max_length=200,unique=True)
 	product_previous_price = models.FloatField(default=0)
 	product_extra_data = JSONField(default={})
 	product_availability = models.CharField(max_length=50)
-	product_image = models.CharField(max_length=200,blank=True)
+	product_image = models.CharField(max_length=500,blank=True)
 
 class PriceDetails(models.Model):
 	price = models.FloatField()
